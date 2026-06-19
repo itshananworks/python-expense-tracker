311bff154b722fd5f22a93efb78d47d5fb29cae2
unknown <itshananworks@gmail.com>
2026-06-19 18:58:21 +0700

    Complete expense tracker v2 with functions

--- main.py
+++ main.py
@@ -1,45 +1,68 @@
 pengeluaran = {}
 
-while True:
+"""TOTAL PENGELUARAN"""
+def total_pengeluaran(pengeluaran):
     total = 0
-    print("=" * 25, "PERSONAL EXPENSE TRACKER", "=" * 25)
-    print("1.Tambah Pengeluaran")
-    print("2.Lihat Pengeluaran")
-    print("3.Total Pengeluaran")
-    print("4.Keluar")
-    pilihan = input("Masukkan Pilihan Anda: ").lower()
-    if pilihan not in [
-        "1", "tambah pengeluaran", "1.tambah pengeluaran",
-        "2", "lihat pengeluaran", "2.lihat pengeluaran",
-        "3", "total pengeluaran", "3.total pengeluaran",
-        "4", "keluar", "4.keluar"
-    ]:
-        print("Pilihan Tidak Valid, Harap Memasukkan Pilihan Yang Valid!")
-        continue
+    if pengeluaran == {}:
+        print("ERROR:Gagal Menjumlahkan Total, Tidak Ada Riwayat Pengeluaran:(")
     else:
-        if pilihan in ["4", "keluar", "4.keluar"]:
-            print("Jangan Lupa Catat Pengeluaranmu Ya:D")
-            break
-        elif pilihan in ["1", "tambah pengeluaran", "1.tambah pengeluaran"]:
-            try:
-                nama_pengeluaran = input("Masukkan Nama Pengeluaran: ")
-                jumlah_pengeluaran = int(input("Masukkan Jumlah Pengeluaran: "))
-                pengeluaran[nama_pengeluaran] = jumlah_pengeluaran
-                print("YEYY Berhasil Mencatat Pengeluaran:D")
-            except ValueError:
-                print("Harap Memasukkan Angka!!")
-        elif pilihan in ["2", "lihat pengeluaran", "2.lihat pengeluaran"]:
-            if pengeluaran == {}:
-                print("Belum Ada Pengeluaran Yang Tercatat:(")
+        for jumlah_pengeluaran in pengeluaran.values():
+            total += jumlah_pengeluaran
+        print("Total pengeluaran Anda:", total)
+
+
+
+"""LIHAT PENGELUARAN"""
+def lihat_pengeluaran(pengeluaran):
+    if pengeluaran == {}:
+        print("Belum Ada Pengeluaran Yang Tercatat:(")
+    else:
+        nomor = 1
+        for nama_pengeluaran, jumlah_pengeluaran in pengeluaran.items():
+            print(nomor,".",nama_pengeluaran, "=", jumlah_pengeluaran)
+            nomor += 1
+
+"""INPUT DAN MENAMBAHKAN PENGELUARAN"""
+def tambah_pengeluaran(pengeluaran):
+    try:
+        nama_pengeluaran = input("Masukkan Nama Pengeluaran: ")
+        jumlah_pengeluaran = int(input("Masukkan Jumlah Pengeluaran: "))
+        pengeluaran[nama_pengeluaran] = jumlah_pengeluaran
+        print("YEYY Berhasil Menambahkan Dan Mencatat Pengeluaran Anda:D")
+    except ValueError:
+        print("Harap Masukkan Angka!")
+
+
+
+
+"""FUNCTION SYSTEM"""
+def main_menu_process():
+    while True:
+            print("=" * 25, "PERSONAL EXPENSE TRACKER", "=" * 25)
+            print("1.Tambah Pengeluaran")
+            print("2.Lihat Pengeluaran")
+            print("3.Total Pengeluaran")
+            print("4.Keluar")
+            pilihan = input("Masukkan Pilihan Anda: ").lower()
+            if pilihan not in [
+                "1", "tambah pengeluaran", "1.tambah pengeluaran",
+                "2", "lihat pengeluaran", "2.lihat pengeluaran",
+                "3", "total pengeluaran", "3.total pengeluaran",
+                "4", "keluar", "4.keluar"
+            ]:
+                print("Pilihan Tidak Valid, Harap Memasukkan Pilihan Yang Valid")
+                continue
             else:
-                nomor = 1
-                for nama_pengeluaran, jumlah_pengeluaran in pengeluaran.items():
-                    print(nomor,".",nama_pengeluaran, "=", jumlah_pengeluaran)
-                    nomor += 1
-        elif pilihan in ["3", "total pengeluaran", "3.total pengeluaran"]:
-            if pengeluaran == {}:
-                print("ERROR:Gagal Menjumlahkan Total, Tidak Ada Riwayat Pengeluaran:(")
-            else:
-                for jumlah_pengeluaran in pengeluaran.values():
-                    total += jumlah_pengeluaran
-                print("Total Pengeluaran Anda:", total)
+                if pilihan in ["4", "keluar", "4.keluar"]:
+                    print("Jangan Lupa Catat Pengeluaranmu Ya:D")
+                    break
+                elif pilihan in ["1", "tambah pengeluaran", "1.tambah pengeluaran"]:
+                    tambah_pengeluaran(pengeluaran)
+                elif pilihan in ["2", "lihat pengeluaran", "2.lihat pengeluaran"]:
+                    lihat_pengeluaran(pengeluaran)
+                elif pilihan in ["3", "total pengeluaran", "3.total pengeluaran"]:
+                    total_pengeluaran(pengeluaran)
+
+
+"""PROSES SYSTEM"""
+main_menu_process()
